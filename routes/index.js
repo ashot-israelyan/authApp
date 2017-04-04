29399821289a12b9dbd722d31822ff47869b17e0
Ashot Israelyan <ashot.israelyan@picsart.com>
2017-04-04 16:44:22 +0400

    locahosl:3000/ cant get bug fixed.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.render('dashboard', {
+      message: 'If you see this page, that means that you are successfully logined'
+    });
+    next();
+  } else {
+    return res.redirect('/users/login');
+  }
+});
+
 router.get('/dashboard', function (req, res, next) {
 
   if (req.isAuthenticated()) {
